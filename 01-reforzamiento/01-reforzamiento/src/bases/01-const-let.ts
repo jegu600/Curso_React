77ae8e3ce90d2b62abfba26b6a116830766741b9
jegu600 <jegu600@gmail.com>
2025-09-15 09:12:45 -0500

    corrigiendo

--- 01-reforzamiento/01-reforzamiento/src/bases/01-const-let.ts
+++
@@ -1,8 +0,0 @@
-const firtsName : string = 'Jhon';
-const lastName = 'Galeano';
-
-let diceNumber = 5;
-diceNumber = 3;
-
-const contieneLetraJ = firtsName.includes('e');
-console.log({firtsName, lastName, diceNumber, contieneLetraJ});
--- 01-reforzamiento/01-reforzamiento/src/bases/02-template-string.ts
+++
@@ -1,6 +0,0 @@
-const firtsName : string = 'Jhon';
-const lastName = 'Galeano';
- 
-const fullName = ` ${firtsName} ${lastName} `;
-
-console.log(`Mi nombre es: ${fullName}`);
