4b47c827aa960bb3b9c76ea57c6c72472603e943
jegu600 <jegu600@gmail.com>
2025-09-21 08:10:44 -0500

    Actulizacion de GitfLis

--- 03-gifs-app/src/gifs/components/GifList.tsx
+++ 03-gifs-app/src/gifs/components/GifList.tsx
@@ -2,15 +2,15 @@
 export const GifList = () => {
   return (
     <div className="gifs-container" >
-  
-        mockGifs.map((git) => (
+
+      {/* mockGifs.map((git) => (
             <div key={git.id} className="gif-card">
                 <img src={git.url} alt={git.title} />
                 <h3>{git.title}</h3>
                 <p>{git.width}x{git.height}</p>
             </div>
-        ))
-   
-</div >
+        )) */}
+
+    </div >
   )
 }
